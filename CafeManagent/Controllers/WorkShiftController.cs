ba536d837100ee9ad23929d80eea02c4e5e2329b
Khoa2305 <dikhoadiem@gmail.com>
2025-10-20 15:36:00 +0700

    Commit

---
+++ CafeManagent/Controllers/WorkShiftController.cs
@@ -0,0 +1,19 @@
+﻿using CafeManagent.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafeManagent.Controllers
+{
+    public class WorkShiftController : Controller
+    {
+        private readonly CafeManagementContext _context;
+        public WorkShiftController(CafeManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
