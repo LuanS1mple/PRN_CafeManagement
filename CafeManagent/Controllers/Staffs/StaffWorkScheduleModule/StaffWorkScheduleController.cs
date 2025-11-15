6a5b5a77ff18a46f8144c35670ca288e1aa9afcb
NgoVanCuongSE181200 <ngovancuong178@gmail.com>
2025-11-15 22:56:21 +0700

    commit

--- CafeManagent/Controllers/Staffs/StaffWorkScheduleModule/StaffWorkScheduleController.cs
+++ CafeManagent/Controllers/Staffs/StaffWorkScheduleModule/StaffWorkScheduleController.cs
@@ -15,9 +15,10 @@ namespace CafeManagent.Controllers.Staffs.StaffWorkScheduleModule
             _service = service;
             _context = context;
         }
-        [HttpGet("StaffWorkSchedule/{staffId}")]
-        public async Task<IActionResult> Index(int staffId)
+        [HttpGet("StaffWorkSchedule")]
+        public async Task<IActionResult> Index()
         {
+            int staffId = HttpContext.Session.GetInt32("StaffId").Value;
             var staffSchedule = await _service.GetWorkShiftsByStaffAsync(staffId);
             ViewBag.StaffId = staffId;
             return View(staffSchedule);
